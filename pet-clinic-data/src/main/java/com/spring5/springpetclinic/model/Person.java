8abd8e6d2ab1ba701cf8d553fb381ff807db6018
user01 <sachin.saraswat1741@gmail.com>
2018-12-30 23:11:19 -0500

    Commit #03

--- pet-clinic-data/src/main/java/com/spring5/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/spring5/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.spring5.springpetclinic.model;
 
-public class Person {
+public class Person extends  BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/spring5/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spring5/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.spring5.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends  BaseEntity {
 
     private PetType petType;
     private Owner owner;
