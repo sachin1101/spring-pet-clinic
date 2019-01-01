207672ef901bc88bebc2e1b4fdbcf4729e73bb11
user01 <sachin.saraswat1741@gmail.com>
2019-01-01 14:25:54 -0500

    Commit #04

--- pet-clinic-web/src/main/java/com/spring5/springpetclinic/Controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/spring5/springpetclinic/Controllers/OwnersController.java
@@ -1,15 +1,26 @@
 package com.spring5.springpetclinic.Controllers;
 
+import com.spring5.springpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/Owners")
 public class OwnersController {
 
+    private OwnerService ownerService;
+
+    @Autowired
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","/index","/index.html"})
-    public  String getOwners()
+    public  String getOwners(Model model)
     {
+        model.addAttribute("owners",this.ownerService.findAll());
         return  "Owners/index.html";
 
     }
