a19882be2378b5117d1d566205e6f0470556f031
user01 <sachin.saraswat1741@gmail.com>
2019-01-01 14:31:55 -0500

    Commit #04

--- pet-clinic-web/src/main/java/com/spring5/springpetclinic/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/spring5/springpetclinic/Controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.spring5.springpetclinic.Controllers;
 
+import com.spring5.springpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String getVetList()
+    public String getVetList(Model model)
     {
+        model.addAttribute("vetList" , vetService.findAll());
         return "vets/index";
     }
 }
